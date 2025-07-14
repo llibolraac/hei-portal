d21e5c82ce65f6e039a3b79f5af993ebe96dd4f6
llibolraac <bcrbaconguis@gmail.com>
2025-07-14 09:24:56 +0800

    Latest Update

--- src/main.js
+++ src/main.js
@@ -13,7 +13,11 @@ import { TailwindPagination } from 'laravel-vue-pagination';
 
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: 'https://ucmis-api.chedcaraga.ph/api', // Adjust as needed
+=======
+  baseURL: 'http://localhost:8000/api/', // Adjust as needed
+>>>>>>> 5a84d48 (Latest Update)
   // other configurations
 });
 
