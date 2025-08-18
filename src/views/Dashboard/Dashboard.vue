e506518b9c4f34fa70617f8e41122a6014fb697c
llibolraac <bcrbaconguis@gmail.com>
2025-08-18 16:51:08 +0800

    Latest Update

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -312,7 +312,7 @@
           </router-link>
         </li>
 
-        <li v-if="userId == 83">
+        <li>
           <router-link
             to="/home/batch_update"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
@@ -343,7 +343,7 @@
           </router-link>
         </li>
 
-        <li v-if="userId == 83">
+        <li>
           <button
             @click="handleSemSY"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
